612d39070e7d2a87ad74ee01469adf62ed90d8a9
Aniket Ghosh <121872510+Aniketghosh2003@users.noreply.github.com>
2024-03-18 01:33:07 +0530

    Day17 uploaded

---
+++ Day17/Implement_queue.c
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h> 
+class Queue {
+    int *arr;
+    int qfront;
+    int rear;
+    int size;
+public:
+    Queue() {
+       size = 100001;
+       arr = new int[size];
+       qfront = 0;
+       rear = 0;
+
+    }
+
+    /*----------------- Public Functions of Queue -----------------*/
+
+    bool isEmpty() {
+        if(qfront == rear){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    void enqueue(int data) {
+        if(rear == size)
+             return;
+        else
+            arr[rear++] = data;     
+    }
+
+    int dequeue() {
+        if(isEmpty())
+           return -1;
+
+        else{
+            int ans = arr[qfront];
+            arr[qfront] = -1;
+            qfront++;
+            if(qfront == rear){
+                qfront = 0;
+                rear = 0;
+            }
+            return ans;
+        }   
+    }
+
+    int front() {
+        if(isEmpty())
+           return -1;
+        else{
+            return arr[qfront];
+
+        }   
+    }
+};
